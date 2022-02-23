baff9895c7b8fa665d9bb1b0df286f8628f0eada
L-e-e-F <2576972210@qq.com>
2022-02-23 22:53:51 +0800

    修改Matches

--- src/main/java/com/example/entity/Matches.java
+++ src/main/java/com/example/entity/Matches.java
@@ -9,15 +9,9 @@ import java.util.Date;
 public class Matches implements Serializable {
     private Long id;
 
-    private Long eventId;
-
     private Date time;
 
-    private Long homeTeamId;
-
     private String homeTeam;
-
-    private Long visitingTeamId;
 
     private String visitingTeam;
 
@@ -38,11 +32,8 @@ public class Matches implements Serializable {
         }
         Matches other = (Matches) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getEventId() == null ? other.getEventId() == null : this.getEventId().equals(other.getEventId()))
             && (this.getTime() == null ? other.getTime() == null : this.getTime().equals(other.getTime()))
-            && (this.getHomeTeamId() == null ? other.getHomeTeamId() == null : this.getHomeTeamId().equals(other.getHomeTeamId()))
             && (this.getHomeTeam() == null ? other.getHomeTeam() == null : this.getHomeTeam().equals(other.getHomeTeam()))
-            && (this.getVisitingTeamId() == null ? other.getVisitingTeamId() == null : this.getVisitingTeamId().equals(other.getVisitingTeamId()))
             && (this.getVisitingTeam() == null ? other.getVisitingTeam() == null : this.getVisitingTeam().equals(other.getVisitingTeam()))
             && (this.getResult() == null ? other.getResult() == null : this.getResult().equals(other.getResult()));
     }
@@ -52,11 +43,8 @@ public class Matches implements Serializable {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getEventId() == null) ? 0 : getEventId().hashCode());
         result = prime * result + ((getTime() == null) ? 0 : getTime().hashCode());
-        result = prime * result + ((getHomeTeamId() == null) ? 0 : getHomeTeamId().hashCode());
         result = prime * result + ((getHomeTeam() == null) ? 0 : getHomeTeam().hashCode());
-        result = prime * result + ((getVisitingTeamId() == null) ? 0 : getVisitingTeamId().hashCode());
         result = prime * result + ((getVisitingTeam() == null) ? 0 : getVisitingTeam().hashCode());
         result = prime * result + ((getResult() == null) ? 0 : getResult().hashCode());
         return result;
@@ -68,11 +56,8 @@ public class Matches implements Serializable {
                 " [" +
                 "Hash = " + hashCode() +
                 ", id=" + id +
-                ", eventId=" + eventId +
                 ", time=" + time +
-                ", homeTeamId=" + homeTeamId +
                 ", homeTeam=" + homeTeam +
-                ", visitingTeamId=" + visitingTeamId +
                 ", visitingTeam=" + visitingTeam +
                 ", result=" + result +
                 ", serialVersionUID=" + serialVersionUID +
